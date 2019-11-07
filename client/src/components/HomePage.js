5e6002a8df7068801a8f55334a1d2513029d0246
Kenneth Pham <kennethpham98@gmail.com>
2019-11-07 10:51:00 -0800

    changed HomePage.js

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import SignUp from "../components/Signup.js"
 
+import {Link, NavLink, Route} from 'react-router-dom'
+import SchedulePage from '../components/SchedulePage'
+
 class HomePage extends React.Component {
     render() {
         return (
@@ -8,6 +11,11 @@ class HomePage extends React.Component {
                 <p>Homepage TODO need to finsh</p>
                 <p>Sticking SignUp component here to check connection to server</p>
                 <SignUp />
+                <Link to="/SchedulePage"> Continue as Guest </Link>
+                <Route 
+                    path ="/SchedulePage">
+                    component={() => <SchedulePage/>}    
+                </Route>
             </div>
         )
     }
