ec3867f89e88916fcceca5e4271266f6bbbb7606
Kenneth Pham <kennethpham98@gmail.com>
2019-11-07 10:50:09 -0800

    added Continue to Guest

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Link} from "react-router-dom";
 import HomePage from "./components/HomePage";
+import SchedulePage from './components/SchedulePage';
 //import NavigationBar from "./components/NavigationBar";
 
 
@@ -10,7 +11,8 @@ class App extends React.Component {
   render() {
     return (
       <Router>
-          <Route path="/" exact component={HomePage} />
+          <Route path="/" exact component={HomePage}/>
+          <Route path="/SchedulePage" exact component={SchedulePage} />
       </Router>
     )
   }
