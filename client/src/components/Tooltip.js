3b0c2b04d207ef81b2181e51e6510a8d55076d38
Jad Barrere <jbarrere@ucsd.edu>
2019-11-08 14:35:24 -0800

    Instructions for use of Tooltip component have been updated.

--- client/src/components/Tooltip.js
+++ client/src/components/Tooltip.js
@@ -1,24 +1,21 @@
 /* Tooltip.js - A tooltip is an info icon with info that appears on hover.
+*       IMPORTANT: The Tooltip REQUIRES a UNIQUE ID as the ID is how it
+*            links the message to the icon.
+*       Tooltip uses the module 'react-tooltip' which may need to be installed.
+*       Info can be declared elsewhere or injected directly.
 * Example use case:
-*
 *---ExampleFile.js---
 *       import Tooltip from './Tooltip'
 *
-*       // IMPORTANT: The tooltip REQUIRES a UNIQUE ID as the ID is how it
-*       //      links the message to the icon.
-*       // Declare the info elsewhere to format it.  If no formatting is needed,
-*       // the info can be injected directly without declaring it elsewhere.
-*
 *       ... (inside JSX)
 *
-*           var ExampleInfo = <div id=ExampleInfoID>
+*           var ExampleInfo = <div>
 *                   <p>First line of text.</p>
 *                   <p>Second line of text.</p>
 *               </div>
 *           <Tooltip id="UniqueTooltipID" info = {ExampleInfo}/>
-*
 *---ExampleFile.css---
-*       .ExampleInfoID {
+*       .UniqueTooltipID {
 *           color: blue;
 *       }
 */
@@ -33,7 +30,7 @@ class Tooltip extends React.Component {
         return (
             <div className="TooltipDiv">
                 <svg>
-                    <circle data-tip data-for={this.props.id} data-multiline="true" cx = "14" cy="14" r="10"/>
+                    <circle data-tip data-for={this.props.id} cx = "14" cy="14" r="10"/>
                     <text data-tip data-for={this.props.id} textAnchor="middle" x="14" y="19">?</text>
                 </svg>
                 <div className="Tooltip">
