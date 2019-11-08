4bdf203c759d565386e4d8e1f588f5af4e5f8cee
nichi97 <30396723+nichi97@users.noreply.github.com>
2019-11-08 12:46:52 -0800

    Finished layout

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,21 +1,71 @@
 import React from 'react';
 import SignUp from "../components/Signup.js"
-
-import {Link, NavLink, Route} from 'react-router-dom'
-import SchedulePage from '../components/SchedulePage'
+import "./homepage.css"
 
 class HomePage extends React.Component {
     render() {
         return (
             <div>
-                <p>Homepage TODO need to finsh</p>
-                <p>Sticking SignUp component here to check connection to server</p>
-                <SignUp />
-                <Link to="/SchedulePage"> Continue as Guest </Link>
-                <Route 
-                    path ="/SchedulePage">
-                    component={() => <SchedulePage/>}    
-                </Route>
+                <div id="page_container">
+                    <header>
+                        <h1> Header </h1>
+                    </header>
+                    <div id="left_sidebar">
+                        <div id="search_input">
+                            <p> Search_input</p>
+                        </div>
+                        <div id="search_result">
+                            <p> Search_result</p>
+                        </div>
+                        <div id="generate">
+                            <p> Generate Btn</p>
+                        </div>
+                        <div id="need_want">
+                            <p> Need vs want</p>
+                        </div>
+                    </div>
+                    <div id="schedule_area">
+                        <div id="preferences">
+                            <p> Preferences</p>
+                        </div>
+                        <div id="sort">
+                            <p> sort_by</p>
+                        </div>
+                        <div id="grid_area">
+                            <p> grid_area</p>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
