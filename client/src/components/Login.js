21c3aaaec8437ffec2aa89a133ff4e7457390767
jeffreyEdgarYeung <57332305+jeffreyEdgarYeung@users.noreply.github.com>
2019-11-17 19:32:24 -0800

    Add files via upload

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,89 +1,137 @@
-import React, {useState} from "react"
-import "./Login.css"
-import "../App.css"
+import React, {useState} from 'react';
+import './Login.css';
 
 export class Login extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
             email: "",
-            password: ""
-        }
+            password: "",
+            cPassword:"",
+            showLogin: true
+        };
+    }
+
+    toggleSignUp() {
+        this.setState({
+            showLogin: false
+        });
+    }
+
+    toggleSignIn() {
+        this.setState({
+            showLogin: true
+        });
     }
 
     render() {
-        const { email, password } = this.state
 
+        const { email, password, cPassword } = this.state;
 
         return (
-            <div className="popup">
-                <div className="popup_inner">
-
-                    <h1>{this.props.text}</h1>
+            <div className='popup'>
 
 
+                <div className='popup_inner'>
 
-                    <button id="loginClose" onClick={this.props.closeLogin}>
+                    <h1>Login / Create account</h1>
+
+
+                    <button id='loginClose' onClick={this.props.closeLogin}>
                         <img src="./Btn.png" width="20px" height="20px" alt="close me" />
                     </button>
 
 
-                    <div className="forms">
-                        <div className="loginForm">
-                            <form> {/*onSubmit={this.handleLogin}*/}
-                                <label htmlFor="email">Email</label>
-                                <input
-                                    name="email"
-                                    type="text"
-                                    placeholder="Enter your email"
-                                    //
-                                    //value={email}
-                                    // onChange={this.handleChange}
-                                />
-                                <label htmlFor="email">Password</label>
-                                <input
-                                    name="password"
-                                    type="password"
-                                    placeholder="Enter your password"
-                                    //value={password}
-                                    //onChange={this.handleChange}
-                                />
-                                <button type="submit">Login</button>
-                            </form>
-                        </div>
-
-                        <div className="vl"></div>
-
-                        <div className="createForm">
-                            <form> {/*onSubmit={this.handleCreate}*/}
-                                <label htmlFor="email">Email</label>
-                                <input
-                                    name="email"
-                                    type="text"
-                                    placeholder="Enter your email"
-                                    //
-                                    //value={email}
-                                    // onChange={this.handleChange}
-                                />
-                                <label htmlFor="email">Password</label>
-                                <input
-                                    name="password"
-                                    type="password"
-                                    placeholder="Enter your password"
-                                    //value={password}
-                                    //onChange={this.handleChange}
-                                />
-                                <button type="submit">Create Account</button>
-                            </form>
-                        </div>
+                    <div className='loginToggle'>
+                        <button onClick={this.toggleSignIn.bind(this)}>Login</button>
+                        <button onClick={this.toggleSignUp.bind(this)}>Sign-up</button>
                     </div>
+
+
+                    {this.state.showLogin ?
+                        <div className='forms'>
+                            <div className='loginForm'>
+                                <form> {/*onSubmit={this.handleLogin}*/}
+
+                                    {/*No for labels?*/}
+                                    {/*<label htmlFor="email">Email</label>*/}
+
+                                    <div className='inputContainer'>
+                                        <input
+                                            name="email"
+                                            type="text"
+                                            placeholder="Enter your email"
+                                            className='input'
+                                            //TODO
+                                            //value={email}
+                                            // onChange={this.handleChange}
+                                        />
+
+                                        {/*Animation border*/}
+                                        <span className='border'></span>
+
+                                    </div>
+                                    {/*<label htmlFor="email">Password</label>*/}
+                                    <input
+                                        name="password"
+                                        type="password"
+                                        placeholder="Enter your password"
+                                        className='input'
+                                        //value={password}
+                                        //onChange={this.handleChange}
+                                    />
+                                    <button type="submit">Login</button>
+                                </form>
+                            </div>
+                        </div>
+
+                        :
+
+                        <div className='forms'>
+
+                            <div className='createForm'>
+                                <form> {/*onSubmit={this.handleCreate}*/}
+                                    {/*<label htmlFor="email">Email</label>*/}
+                                    <input
+                                        name="email"
+                                        type="text"
+                                        placeholder="Enter your email"
+                                        className='input'
+                                        //
+                                        //value={email}
+                                        // onChange={this.handleChange}
+                                    />
+                                    {/*<label htmlFor="email">Password</label>*/}
+                                    <input
+                                        name="password"
+                                        type="password"
+                                        placeholder="Enter your password"
+                                        className='input'
+                                        //value={password}
+                                        //onChange={this.handleChange}
+                                    />
+                                    {/*<label htmlFor="email">Confirm Password</label>*/}
+                                    <input
+                                        name="cPassword"
+                                        type="password"
+                                        placeholder="Confirm your password"
+                                        //value={password}
+                                        //onChange={this.handleChange}
+                                    />
+                                    <button type="submit">Create Account</button>
+                                </form>
+                            </div>
+
+
+                        </div>
+                    }
 
 
 
                 </div>
             </div>
-        )
+        );
     }
 }
 
-export default Login
+export default Login;
