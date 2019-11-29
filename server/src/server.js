5d52d60f49cdf69963609f70e59515f94d9f1386
Kaylyn Han <kaylynhan@MacBook-Pro.local>
2019-11-29 15:38:31 -0800

    removed res.sendFile bc I think express should be handling that for us

--- server/src/server.js
+++ server/src/server.js
@@ -15,13 +15,13 @@ const uri = process.env.PROD_MONGODB || 'mongodb://127.0.0.1:27017/asappdb';
 app.use(cors());
 app.use(bodyParser.json());
 
-if (process.env.NODE_ENV === 'production'){
-    app.use(express.static('../client/build/'));
+// if (process.env.NODE_ENV === 'production'){
+//     app.use(express.static('../client/build/'));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname + '../../client/build/index.html'));
-    })
-}
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname + '../../client/build/index.html'));
+//     })
+// }
 
 mongoose.connect(uri, { useNewUrlParser: true });
 const connection = mongoose.connection;
